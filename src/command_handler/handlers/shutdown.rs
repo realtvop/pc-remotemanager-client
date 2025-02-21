cf3a40291440fb2e37679702fe3183575483d23e
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-02-22 02:53:10 +0800

    Refactor shutdown handler to correctly pass message parameter as a reference

--- src/command_handler/handlers/shutdown.rs
+++ src/command_handler/handlers/shutdown.rs
@@ -24,26 +24,26 @@ pub fn shutdown_handler(data: Value) -> Result<(), Box<dyn Error>> {
     let params: ShutdownParams = serde_json::from_value(data)?;
     println!("Handling shutdown command: {:?}", params);
     
-    let msg = params.message.unwrap_or_default().as_str() ?? "";
+    let msg = params.message.unwrap_or_default();
     match params.action_type {
         ActionType::Immediately => {
             println!("Shutting down immediately");
             Command::new("shutdown")
-                .args(["/s", "/t", "0", "/c", msg])
+                .args(["/s", "/t", "0", "/c", &msg])
                 .spawn()?;
         },
         ActionType::Scheduled => {
             if let Some(interval) = params.interval {
                 println!("Scheduling shutdown in {} seconds", interval);
                 Command::new("shutdown")
-                    .args(["/s", "/t", &interval.to_string(), "/c", msg])
+                    .args(["/s", "/t", &interval.to_string(), "/c", &msg])
                     .spawn()?;
             }
         },
         ActionType::Abort => {
             println!("Clearing scheduled shutdown");
             Command::new("shutdown")
-                .args(["/a", "/c", msg])
+                .args(["/a", "/c", &msg])
                 .spawn()?;
         }
     }
