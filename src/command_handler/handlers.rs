1c8fa470657cc9b6748d676b3b8803e4f605bee8
飞机上的长电视 <realtvop@realtvop.top>
2025-03-03 13:26:39 +0800

    Update handlers.rs

--- src/command_handler/handlers.rs
+++ src/command_handler/handlers.rs
@@ -1,6 +1,8 @@
 mod shutdown;
 mod keysimulator;
+mod cmdhandler;
 
 pub use shutdown::shutdown_handler;
 pub use keysimulator::keyboard_handler;
 pub use keysimulator::input_handler;
+pub use cmdhandler::command_handler
