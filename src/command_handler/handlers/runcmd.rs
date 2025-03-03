984e6e984c7e02eb8e6812040de774830a50d35c
飞机上的长电视 <realtvop@realtvop.top>
2025-03-03 13:26:00 +0800

    Update runcmd.rs

---
+++ src/command_handler/handlers/runcmd.rs
@@ -0,0 +1,19 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::error::Error;
+use std::process::Command;
+
+#[derive(Debug, Deserialize)]
+struct CommandParams {
+    command: String,
+}
+
+pub fn command_handler(data: Value) -> Result<(), Box<dyn Error>> {
+    let params: CommandParams = serde_json::from_value(data)?;
+    println!("Handling command: {:?}", params);
+    
+    println!("Running: {}", interval);
+        Command::new(params.command).spawn()?;
+
+    Ok(())
+}
